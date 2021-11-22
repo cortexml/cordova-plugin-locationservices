0703abafe7f98a58088a2ad1f4fcc5215a9e64a7
cortexml <gracegeeg@gmail.com>
2021-11-22 22:21:58 +0900

    test

--- src/android/CordovaLocationServices.java
+++ src/android/CordovaLocationServices.java
@@ -29,6 +29,7 @@ import android.util.Log;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
